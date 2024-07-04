9b01b3e26373a18e6445b22ca92117e9dbc22923
Kir4h <kpotutinskiy@mail.ru>
2024-07-05 03:11:42 +0700

    New Card
    Added "Rat"

---
+++ LLK Cards/LLK Cards/LLK Cards.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using LLKCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace LLKCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class LLKCards : BaseUnityPlugin
+    {
+        private const string ModId = "com.LLK.rounds.LLKCards";
+        private const string ModName = "LLK Cards";
+        public const string Version = "0.1.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "LLK";
+        public static LLKCards instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<Rat>();
+        }
+    }
+}
